f19eac08ac9f964dcbf837e9307edcb762f69d3c
foxydot <catherine@madsciencedept.com>
2015-12-17 20:36:54 +0000

    stage server version

--- assets/themes/msd-genesis-child/lib/js/homepage-jquery.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    var numwidgets = $('#homepage-widgets section.widget').length;
-    $('#homepage-widgets').addClass('cols-'+numwidgets);
-    var cols = 12/numwidgets;
-    $('#homepage-widgets section.widget').addClass('col-sm-'+cols);
-    $('#homepage-widgets section.widget').addClass('col-xs-12');
-});
--- assets/themes/msd-genesis-child/lib/js/ie-jquery.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function($) {	
-    $('.header-image .site-title a').css( "background-size", "contain" );
-	$('.no-csscolumns .footer-widgets-1 .menu').columnize({ columns: 3 });
-});
