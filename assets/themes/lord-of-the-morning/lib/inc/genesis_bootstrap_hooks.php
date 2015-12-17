69365dd47dc02515a611e3a29c45a405b49cfc16
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-12-17 16:36:09 -0500

    bean bullets, about page weirdness

--- assets/themes/lord-of-the-morning/lib/inc/genesis_bootstrap_hooks.php
+++ assets/themes/lord-of-the-morning/lib/inc/genesis_bootstrap_hooks.php
@@ -46,7 +46,7 @@ function msdlab_bootstrap_content( $attributes ){
     switch($layout){
         case 'content-sidebar':
         case 'sidebar-content':
-            $attributes['class'] .= ' col-md-8 col-sm-12';
+            $attributes['class'] .= ' col-md-9 col-sm-12';
             break;
         case 'content-sidebar-sidebar':
         case 'sidebar-sidebar-content':
@@ -66,7 +66,7 @@ function msdlab_bootstrap_sidebar( $attributes ){
     switch($layout){
         case 'content-sidebar':
         case 'sidebar-content':
-            $attributes['class'] .= ' col-md-4 hidden-sm hidden-xs';
+            $attributes['class'] .= ' col-md-3 hidden-sm hidden-xs';
             break;
         case 'content-sidebar-sidebar':
         case 'sidebar-sidebar-content':
