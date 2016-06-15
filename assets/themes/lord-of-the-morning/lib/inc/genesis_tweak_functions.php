78729f8632929bf27e7451f6b4fc03b3c4962df3
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-06-14 22:46:28 -0400

    menu fix trial 1

--- assets/themes/lord-of-the-morning/lib/inc/genesis_tweak_functions.php
+++ assets/themes/lord-of-the-morning/lib/inc/genesis_tweak_functions.php
@@ -186,7 +186,6 @@ function msdlab_do_nav() {
     genesis_nav_menu( array(
         'theme_location' => 'primary',
         'menu_class'     => $class,
-        //'walker' => new Description_Walker,
     ) );
 
 }
@@ -446,7 +445,7 @@ register_nav_menus( array(
     'footer_menu' => 'Footer Menu'
 ) );
 function msdlab_do_footer_menu(){
-    if(has_nav_menu('footer_menu')){$footer_menu = wp_nav_menu( array( 'theme_location' => 'footer_menu','container_class' => 'ftr-menu ftr-links','echo' => FALSE, 'walker' => new Description_Walker ) );}
+    if(has_nav_menu('footer_menu')){$footer_menu = wp_nav_menu( array( 'theme_location' => 'footer_menu','container_class' => 'ftr-menu ftr-links','echo' => FALSE, ) );}
     print '<div id="footer_menu" class="footer-menu"><div class="wrap">'.$footer_menu.'</div></div>';
 }
 
@@ -460,139 +459,7 @@ function msdlab_maybe_structural_wrap($context = '', $output = 'open', $echo = t
     }
 }
 
-/**
- * Create HTML list of nav menu items.
- * Replacement for the native Walker, using the description.
- *
- * @see    http://wordpress.stackexchange.com/q/14037/
- * @author toscho, http://toscho.de
- */
-class Description_Walker extends Walker_Nav_Menu
-{
-        /**
-     * Starts the list before the elements are added.
-     *
-     * @see Walker::start_lvl()
-     *
-     * @since 3.0.0
-     *
-     * @param string $output Passed by reference. Used to append additional content.
-     * @param int    $depth  Depth of menu item. Used for padding.
-     * @param array  $args   An array of arguments. @see wp_nav_menu()
-     */
-    function start_lvl( &$output, $depth = 0, $args = array() ) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "\n$indent<ul class=\"sub-menu\">\n";
-    }
 
-    /**
-     * Ends the list of after the elements are added.
-     *
-     * @see Walker::end_lvl()
-     *
-     * @since 3.0.0
-     *
-     * @param string $output Passed by reference. Used to append additional content.
-     * @param int    $depth  Depth of menu item. Used for padding.
-     * @param array  $args   An array of arguments. @see wp_nav_menu()
-     */
-    function end_lvl( &$output, $depth = 0, $args = array() ) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "$indent</ul>\n";
-        if($depth==0){
-            $output .= "$indent</div>\n";
-        }
-    }
-     /**
-     * Start the element output.
-     *
-     * @param  string $output Passed by reference. Used to append additional content.
-     * @param  object $item   Menu item data object.
-     * @param  int $depth     Depth of menu item. May be used for padding.
-     * @param  array $args    Additional strings.
-     * @return void
-     */
-    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 )
-    {
-        $classes     = empty ( $item->classes ) ? array () : (array) $item->classes;
-
-        $class_names = join(
-            ' '
-        ,   apply_filters(
-                'nav_menu_css_class'
-            ,   array_filter( $classes ), $item
-            )
-        );
-
-        ! empty ( $class_names )
-            and $class_names = ' class="'. esc_attr( $class_names ) . '"';
-
-        $output .= "<li id='menu-item-$item->ID' $class_names>";
-
-        $attributes  = '';
-
-        ! empty( $item->attr_title )
-            and $attributes .= ' title="'  . esc_attr( $item->attr_title ) .'"';
-        ! empty( $item->target )
-            and $attributes .= ' target="' . esc_attr( $item->target     ) .'"';
-        ! empty( $item->xfn )
-            and $attributes .= ' rel="'    . esc_attr( $item->xfn        ) .'"';
-        ! empty( $item->url )
-            and $attributes .= ' href="'   . esc_attr( $item->url        ) .'"';
-
-        // insert description for top level elements only
-        // you may change this
-        $description = ( ! empty ( $item->description ) and 0 == $depth )
-            ? '<div class="sub-menu-description">' . esc_attr( $item->description ) . '</div>' : '';
-        $image = ( has_post_thumbnail($item->ID) and 0 == $depth )
-            ? '<div class="sub-menu-image">' . get_the_post_thumbnail($item->ID) . '</div>' : '';
-        $title = apply_filters( 'the_title', $item->title, $item->ID );
-        
-        if($depth == 0){
-            $item_output = $args->before
-            . "<a $attributes>"
-            . $args->link_before
-            . $title
-            . '</a> '
-            . $args->link_after
-            . '<div class="sub-menu-wrap">'
-            . $description
-            . $args->after;
-        } else {
-            $item_output = $args->before
-            . "<a $attributes>"
-            . $args->link_before
-            . $title
-            . '</a> '
-            . $args->link_after
-            . $args->after;
-        }
-
-        // Since $output is called by reference we don't need to return anything.
-        $output .= apply_filters(
-            'walker_nav_menu_start_el'
-        ,   $item_output
-        ,   $item
-        ,   $depth
-        ,   $args
-        );
-    }
-/**
-     * Ends the element output, if needed.
-     *
-     * @see Walker::end_el()
-     *
-     * @since 3.0.0
-     *
-     * @param string $output Passed by reference. Used to append additional content.
-     * @param object $item   Page data object. Not used.
-     * @param int    $depth  Depth of page. Not Used.
-     * @param array  $args   An array of arguments. @see wp_nav_menu()
-     */
-    function end_el( &$output, $item, $depth = 0, $args = array() ) {
-        $output .= "</li>\n";
-    }
-}
 
 /*** SITEMAP ***/
 function msdlab_sitemap(){
